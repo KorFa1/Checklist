ea0c7717eee476e5bd4efc3550acb8699d0fcb64
Kirill Sofrin <sofrin2013@gmail.com>
2022-08-22 13:56:55 +0300

    Adding text field and button functionality

--- Checklists/AddItemTableViewController.swift
+++ Checklists/AddItemTableViewController.swift
@@ -7,12 +7,20 @@
 
 import UIKit
 
-class AddItemTableViewController: UITableViewController {
-
+class AddItemTableViewController: UITableViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
-        
         super.viewDidLoad()
         navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
     }
     
     @IBAction func cancel() {
@@ -21,6 +29,32 @@ class AddItemTableViewController: UITableViewController {
     
     @IBAction func done() {
         navigationController?.popViewController(animated: true)
+        print("contents of the text field: \(textField.text!)")
+    }
+    
+    // MARK: - Table View Delegates
+    
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneBarButton.isEnabled = false
+        return true
+    }
+    
+    // MARK: - Text Field Delegates
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in: oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        if newText.isEmpty {
+            doneBarButton.isEnabled = false
+        } else {
+            doneBarButton.isEnabled = true
+        }
+        return true
     }
     
 }
