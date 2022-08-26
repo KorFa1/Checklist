73606d46765b81ed48a519114cd9b18c69eace09
Kirill Sofrin <sofrin2013@gmail.com>
2022-08-26 18:08:06 +0300

    Adding user defaults to remembering the last open list

--- Checklists/View Controllers/AllListsViewController.swift
+++ Checklists/View Controllers/AllListsViewController.swift
@@ -7,9 +7,11 @@
 
 import UIKit
 
-class AllListsViewController: UITableViewController, ListDetailViewControllerDelegate {
+class AllListsViewController: UITableViewController, ListDetailViewControllerDelegate, UINavigationControllerDelegate{
     
     var dataModel: DataModel!
+    
+    let cellIdentifier = "ChecklistCell"
     
     // MARK: - List Detail View Controller Delegates
     
@@ -38,8 +40,13 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
         navigationController?.popViewController(animated: true)
     }
     
+    // MARK: - Navigation Controller Delwgates
     
-    let cellIdentifier = "ChecklistCell"
+    func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
+        if viewController === self {
+            dataModel.indexOfSelectedChecklist = -1
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -66,6 +73,18 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
 //        }
         
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        navigationController?.delegate = self
+        
+        let index = dataModel.indexOfSelectedChecklist
+        if index >= 0 && index < dataModel.lists.count {
+            let checklist = dataModel.lists[index]
+            performSegue(withIdentifier: "ShowChecklist", sender: checklist)
+        }
+    }
 
     // MARK: - Table view data source
 
@@ -91,6 +110,8 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
     // MARK: - Table view delegate
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        dataModel.indexOfSelectedChecklist = indexPath.row
         let checklist = dataModel.lists[indexPath.row]
         performSegue(withIdentifier: "ShowChecklist", sender: checklist)
     }
